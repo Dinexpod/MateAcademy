b4020a950ece4f300a98ae11771fc5c3ed153690
Anatolii <dinexpod@gmail.com>
2018-11-22 20:59:44 +0200

    improve code readability

--- src/lessons/ls1/task1Sort/InsertSort.java
+++ src/lessons/ls1/task1Sort/InsertSort.java
@@ -7,6 +7,7 @@ public class InsertSort {
         for (int i = 0; i < arr.length; i++) {
             int copyNumber = arr[i];
             int j = i;
+
             while (j > 0 && copyNumber < arr[j-1]) {
                 arr[j] = arr[j-1];
                 j--;
