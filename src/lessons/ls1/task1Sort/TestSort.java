f42eafa790f40f0af33d7064ae8db16e243386c4
Anatolii <dinexpod@gmail.com>
2018-11-22 22:27:52 +0200

    - improve readability - improve output - added "Math.random" for array filling

--- src/lessons/ls1/task1Sort/TestSort.java
+++ src/lessons/ls1/task1Sort/TestSort.java
@@ -3,12 +3,25 @@ package lessons.ls1.task1Sort;
 public class TestSort {
     public static void main(String[] args) {
 
-        int[] test = {10, 9, 10, 7, 6, 5, 4, 3, 2, 1};
+        int[] testMas1 = {10, 9, 10, 7, 6, 5, 4, 3, 2, 1};
+        int[] testMas2 = new int[10];
 
-        int[] copyTest = InsertSort.sort(test);
+        // Заповнення масиву рандомно від -50 до 50
+        for (int i = 0; i < testMas2.length; i++) {
+            testMas2[i] = (int) ((Math.random() * 101) - 50);
+        }
 
-        for (int i : copyTest) {
-            System.out.print(i + " ");
+        int[] copyTestMas1 = InsertSort.sort(testMas1);     //сортування вставками
+        int[] copyTestMas2 = InsertSort.sort(testMas2);     //сортування бульбашкою
+
+        System.out.println("\nСортуваний масив заповнений рандомними числами:");
+        for (int i : copyTestMas2) {
+            System.out.print(i + "\t");
+        }
+
+        System.out.println("\n\nСортуваний масив заповнений вручну:");
+        for (int i : copyTestMas1) {
+            System.out.print(i + "\t");
         }
     }
 }
