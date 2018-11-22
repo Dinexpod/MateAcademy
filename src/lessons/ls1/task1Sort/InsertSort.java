2230034a39b764c3ec9323fc48e8922333e7ece7
Anatolii <dinexpod@gmail.com>
2018-11-22 19:21:14 +0200

    added realization /InsertSort/

--- src/lessons/ls1/task1Sort/InsertSort.java
+++ src/lessons/ls1/task1Sort/InsertSort.java
@@ -1,4 +1,18 @@
 package lessons.ls1.task1Sort;
 
 public class InsertSort {
+
+    public static int[] sort(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            int copyNumber = arr[i];
+            int j = i;
+            while (j > 0 && copyNumber < arr[j-1]) {
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = copyNumber;
+        }
+        return arr;
+    }
 }
--- src/lessons/ls1/task1Sort/TestSort.java
+++ src/lessons/ls1/task1Sort/TestSort.java
@@ -3,9 +3,9 @@ package lessons.ls1.task1Sort;
 public class TestSort {
     public static void main(String[] args) {
 
-       int[] test = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+       int[] test = {10, 9, 10, 7, 6, 5, 4, 3, 2, 1};
 
-        int[] copyTest = BubbleSort.sort(test);
+        int[] copyTest = InsertSort.sort(test);
 
         for (int i : copyTest) {
             System.out.print(i + " ");
