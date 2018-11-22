fd5228609a952740610f30978e3d157698939f75
Anatolii <dinexpod@gmail.com>
2018-11-22 16:03:17 +0200

    Added realization BubbleSort

--- src/lessons/ls1/task1Sort/BubbleSort.java
+++ src/lessons/ls1/task1Sort/BubbleSort.java
@@ -1,4 +1,20 @@
 package lessons.ls1.task1Sort;
 
 public class BubbleSort  {
+
+    public static int[] sort(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+
+            }
+        }
+
+        return arr;
+    }
 }
--- src/lessons/ls1/task1Sort/TestSort.java
+++ src/lessons/ls1/task1Sort/TestSort.java
@@ -3,5 +3,14 @@ package lessons.ls1.task1Sort;
 public class TestSort {
     public static void main(String[] args) {
 
+       int[] test = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+        test = BubbleSort.sort(test);
+
+        for (int i : test) {
+            System.out.print(i + " ");
+        }
+
+
     }
 }
