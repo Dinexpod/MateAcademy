d36d4840e921014b6924bf27f2e513353378c6cc
Anatolii <dinexpod@gmail.com>
2018-11-23 14:45:43 +0200

    - improve code readability

--- src/homeworks/hw1/oneElvisTask/ElvisTask.java
+++ src/homeworks/hw1/oneElvisTask/ElvisTask.java
@@ -4,9 +4,10 @@ public class ElvisTask {
     public static void main(String[] args) {
         int score1, score2;
 
-        score1 = 2;//(int) (Math.random()*11);
-        score2 = 1;//(int) (Math.random()*11);
+        score1 = 2; //(int) (Math.random()*11);
+        score2 = 1; //(int) (Math.random()*11);
 
+        // Метод який приймає рахунок команди: (score1:score2), та ставку спостерігача: (setScore1:setScore2)
         int resultVangas = GameResult.result(score1, score2, 3, 2);
 
         System.out.println("Результат команди: " + score1 + ":" + score2);
