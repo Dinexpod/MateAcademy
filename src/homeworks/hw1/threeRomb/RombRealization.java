7dc6ff2b3805b029462bb2eebad04721219cdbf2
Anatolii <dinexpod@gmail.com>
2018-11-23 18:47:16 +0200

    - added task Romb but it dont work yet

--- src/homeworks/hw1/threeRomb/RombRealization.java
+++ src/homeworks/hw1/threeRomb/RombRealization.java
@@ -6,8 +6,18 @@ public class RombRealization {
 
         for (int i = 0; i < sizeDiagonal; i++) {
             for (int j = 0; j < sizeDiagonal; j++) {
-
+                if(j > sizeDiagonal/2) {
+                    tmp[i][j] = " ";
+                } else {
+                    tmp[i][j] = "*";
+                }
             }
+        }
+        for (int i = 0; i < tmp.length; i++) {
+            for (int j = 0; j < tmp.length; j++) {
+                System.out.print(tmp[i][j]);
+            }
+            System.out.println();
         }
     }
 }
