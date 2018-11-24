7bf6609a082c413b06fe5015d503e691e260d926
Anatolii <dinexpod@gmail.com>
2018-11-24 14:27:16 +0200

    - added to  task "Romb" the "InputReader"

--- src/homeworks/hw1/threeRomb/Romb.java
+++ src/homeworks/hw1/threeRomb/Romb.java
@@ -1,7 +1,16 @@
 package homeworks.hw1.threeRomb;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Romb {
-    public static void main(String[] args) {
-        RombRealization.rombRealization(13);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Input romb size diagonal:");
+        int sizeDiagonal = Integer.parseInt(reader.readLine());
+
+        RombRealization.rombRealization(sizeDiagonal);
     }
 }
