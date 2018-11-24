183df451d89f9727b8685a10a96ef56af83a2b75
Anatolii <dinexpod@gmail.com>
2018-11-24 19:55:01 +0200

    - added code realization task Files

--- src/homeworks/hw1/fiveFiles/Files.java
+++ src/homeworks/hw1/fiveFiles/Files.java
@@ -1,4 +1,30 @@
 package homeworks.hw1.fiveFiles;
 
+import homeworks.hw1.twoMatrix.MatrixRevers;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Files {
+    public static void main(String[] args) {
+        int[][] actualMatrix = {  {0, 1, 2, 3, 4},
+                                 {5, 6, 7, 8, 9},
+                                 {10, 11, 12, 13, 14},
+                                 {15, 16, 17, 18, 19},
+                                 {20, 21, 22, 23, 24} };
+
+        int[][] reversMatrix = MatrixRevers.reverse(actualMatrix);
+
+
+        try(FileWriter writer = new FileWriter("src/homeworks/hw1/fiveFiles/reflected_matrix.txt", false)) {
+            for (int i = 0; i < reversMatrix.length; i++) {
+                for (int j = 0; j < reversMatrix[i].length; j++) {
+                    writer.write(reversMatrix[i][j] + "\t");
+                }
+                writer.write("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
