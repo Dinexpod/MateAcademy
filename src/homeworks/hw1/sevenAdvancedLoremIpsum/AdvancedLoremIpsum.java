0bf294d995ffda7f6f63e66c4e44aec387d1b640
Anatolii <dinexpod@gmail.com>
2018-11-25 02:24:31 +0200

    - improve task seven Advanced Lorem(It works!!!)

--- src/homeworks/hw1/sevenAdvancedLoremIpsum/AdvancedLoremIpsum.java
+++ src/homeworks/hw1/sevenAdvancedLoremIpsum/AdvancedLoremIpsum.java
@@ -7,6 +7,7 @@ public class AdvancedLoremIpsum {
 
         String textFromInput = "";
 
+        //read text from file
         try(BufferedReader reader = new BufferedReader(
                             new InputStreamReader(
                                     new FileInputStream("src/homeworks/hw1/sevenAdvancedLoremIpsum/input.txt")))) {
@@ -19,16 +20,22 @@ public class AdvancedLoremIpsum {
             e.printStackTrace();
         }
 
-        //textFromInput = textFromInput.replaceAll("stop" ,"");
-        textFromInput = textFromInput.replaceAll("\\b\\w{3,3}\\b", "");
-        System.out.println(textFromInput);
+        //delete all except letters
+        textFromInput = textFromInput.replaceAll("[.,\\d]", "");
 
-//        try(FileWriter writer = new FileWriter(
-//                "src/homeworks/hw1/sevenAdvancedLoremIpsum/filtered_lorem_ipsum.txt")) {
-//             writer.write(textFromInput);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        ////delete all words than theirs length > 3
+        textFromInput = textFromInput.replaceAll("\\b[\\w]{0,4}\\b", "");
+
+        ////delete all  "space" that repeat
+        textFromInput = textFromInput.replaceAll("[\\W]{1,10}", " ");
+
+        //write text to file
+        try(FileWriter writer = new FileWriter(
+                "src/homeworks/hw1/sevenAdvancedLoremIpsum/filtered_lorem_ipsum.txt")) {
+             writer.write(textFromInput);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
