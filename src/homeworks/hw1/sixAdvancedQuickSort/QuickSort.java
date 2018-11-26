cbff63c4a673c7cbf95c1980e2db53bf928d9085
Anatolii <dinexpod@gmail.com>
2018-11-27 00:13:22 +0200

    - added check size methods time

--- src/homeworks/hw1/sixAdvancedQuickSort/QuickSort.java
+++ src/homeworks/hw1/sixAdvancedQuickSort/QuickSort.java
@@ -10,14 +10,19 @@ public class QuickSort {
         for (int i = 0; i < actualArray.length; i++) {
             actualArray[i] = (int) ((Math.random()*101)-50);
         }
+        long start = System.currentTimeMillis();
 
         actualArray = QuickSortRealization.doQuickSort(actualArray, 0, actualArray.length - 1);
+
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+
 
         // output sorted array
 //        for (int i = 0; i < actualArray.length; i++) {
 //            System.out.print(actualArray[i] + " ");
 //        }
 
-        System.out.println("\nTHE END");
+        System.out.println(timeConsumedMillis);
     }
 }
--- src/lessons/ls1/task1Sort/TestSort.java
+++ src/lessons/ls1/task1Sort/TestSort.java
@@ -4,24 +4,30 @@ public class TestSort {
     public static void main(String[] args) {
 
         int[] testMas1 = {10, 9, 10, 7, 6, 5, 4, 3, 2, 1};
-        int[] testMas2 = new int[10];
+        int[] testMas2 = new int[1000000];
 
         // Filling array random numbers from -50 до 50
         for (int i = 0; i < testMas2.length; i++) {
             testMas2[i] = (int) ((Math.random() * 101) - 50);
         }
 
-        int[] copyTestMas1 = InsertSort.sort(testMas1);     //insert sort
+//        int[] copyTestMas1 = InsertSort.sort(testMas1);     //insert sort
+
+        long start = System.currentTimeMillis();
         int[] copyTestMas2 = InsertSort.sort(testMas2);     //bubble sort
+        long end = System.currentTimeMillis();
+        long resultTime = (end - start);
 
-        System.out.println("\nBubble sort(randome numbers):");
-        for (int i : copyTestMas2) {
-            System.out.print(i + "\t");
-        }
+        System.out.println(resultTime);
 
-        System.out.println("\n\nInsert sort(randome numbers):");
-        for (int i : copyTestMas1) {
-            System.out.print(i + "\t");
-        }
+//        System.out.println("\nBubble sort(randome numbers):");
+//        for (int i : copyTestMas2) {
+//            System.out.print(i + "\t");
+//        }
+
+//        System.out.println("\n\nInsert sort(randome numbers):");
+//        for (int i : copyTestMas1) {
+//            System.out.print(i + "\t");
+//        }
     }
 }
