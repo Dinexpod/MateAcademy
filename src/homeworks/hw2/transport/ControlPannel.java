bdfce853f7e3e170abccf8095db4a50a1834edc1
Anatolii <dinexpod@gmail.com>
2018-12-01 11:20:16 +0200

    - added and improve realizations in main class

--- src/homeworks/hw2/transport/ControlPannel.java
+++ src/homeworks/hw2/transport/ControlPannel.java
@@ -1,12 +1,45 @@
 package homeworks.hw2.transport;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ControlPannel {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello!!! Welcome to my program 'Car'!!! \nLets try create and use our Car! ");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("That to create a 'Car' push number -> 1.");
+        Integer tmp = Integer.parseInt(reader.readLine());
+
+        if (tmp == 1) {
+            System.out.println("OK!");
+        } else {
+            System.out.println("You choose not founded command!");
+            return;
+        }
+
         Car car = new Car();
-        CarDoor carDoor1 = new CarDoor();
-        car.carDoors.add(carDoor1);
-        CarDoor carDoor = car.getDoorByIndex(0);
-        System.out.println(carDoor);
-        carDoor.getDoorState();
+
+        System.out.println("Cool! You created car, but it not working yet. \n" +
+                "That to see state of car push number -> 1");
+
+        tmp = Integer.parseInt(reader.readLine());
+
+        if (tmp == 1) {
+            car.dataAboutCar();
+        } else {
+            System.out.println("You choose not founded command!");
+            return;
+        }
+
+
+
+//        CarDoor carDoor1 = new CarDoor();
+//        car.carDoors.add(carDoor1);
+//        CarDoor carDoor = car.getDoorByIndex(0);
+//        System.out.println(carDoor);
+//        carDoor.getDoorState();
     }
 }
