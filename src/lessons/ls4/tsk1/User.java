a25160108523397ce700e37b6a6544dde3cf1aaa
Anatolii <dinexpod@gmail.com>
2018-12-02 13:11:01 +0200

    - changet "dateOfProduction" to "int" - changed getting date

--- src/lessons/ls4/tsk1/User.java
+++ src/lessons/ls4/tsk1/User.java
@@ -1,5 +1,7 @@
 package lessons.ls4.tsk1;
 
+import java.util.Objects;
+
 public class User {
     public String name;
     public int age;
@@ -10,6 +12,7 @@ public class User {
         this.age = age;
         this.street = street;
     }
+/*
     @Override
     public boolean equals(Object o) {
         if (o == null) {
@@ -24,8 +27,31 @@ public class User {
 
         User user = (User) o;
 
-//         if (name !)
+        if (this.age != user.age) {
+            return false;
+        }
+        if ((this.street != null) ? (!this.street.equals(user.street)) : (user.street != null))
+            return false;
 
-        return false;
+        if ((this.name != null) ? (!this.name.equals(user.name)) : (user.name != null))
+            return false;
+
+        return true;
+    } */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(street, user.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, street);
     }
 }
+
