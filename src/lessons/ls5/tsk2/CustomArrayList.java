6ffe0753c8b6f4e52c44a12008dfe69f5cc015e8
Anatolii <dinexpod@gmail.com>
2018-12-04 23:15:20 +0200

    - started realization "ArrayList"

---
+++ src/lessons/ls5/tsk2/CustomArrayList.java
@@ -0,0 +1,33 @@
+package lessons.ls5.tsk2;
+
+import java.util.List;
+
+public class CustomArrayList<T> {
+    private Object[] data;
+    private int size;
+
+    public CustomArrayList() {
+        data = new Object[10];
+    }
+
+    public CustomArrayList(int capacity) {
+        if(capacity < 0) {
+            throw new IllegalArgumentException("Cannot construct CustomArrayList with negative capacity!!");
+        }
+
+        data = new Object[capacity];
+    }
+
+    public void add(T value) {
+        ensureCapacity(size + 1);
+        this.data[size] = value;
+        size++;
+    }
+
+    private void ensureCapacity(int capacity) {
+
+        if (data.length < capacity) {
+            Object[] tmpData = new Object[data.length * 2];
+        }
+    }
+}
