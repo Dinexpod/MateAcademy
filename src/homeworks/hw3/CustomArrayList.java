e7aa4f3aa67e95c7d412a3d21486c70ea12f9601
Anatolii <dinexpod@gmail.com>
2018-12-06 15:35:02 +0200

    - finished realization customArrayList

--- src/homeworks/hw3/CustomArrayList.java
+++ src/homeworks/hw3/CustomArrayList.java
@@ -1,8 +1,18 @@
 package homeworks.hw3;
 
-public class CustomArrayList<T> {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * My method customArrayList that overrides some functions in ArrayList
+ *
+ * @param <T>
+ */
+public class CustomArrayList<T> implements List<T> {
     private Object[] data;
-    private int index;
+    private int size;
 
     public CustomArrayList() {
         data = new Object[10];
@@ -16,15 +26,40 @@ public class CustomArrayList<T> {
         data = new Object[capacity];
     }
 
-    public void add(T value) {
-        ensureCapacity(index + 1);
-        this.data[index] = value;
-        index++;
+    public boolean add(T value) {
+        ensureCapacity(size + 1);
+
+        this.data[size] = value;
+        size++;
+        return true;
+    }
+
+    public void add(int index, T value) {
+        if (index < 0 && index >= size) {
+            throw new IllegalArgumentException("Cannot construct CustomArrayList with negative size!");
+        }
+
+        ensureCapacity(size + 1);
+
+        Object[] tmpArray = new Object[size - index];
+
+        for (int i = 0; i < tmpArray.length; i++) {
+            tmpArray[i] = data[index];
+        }
+
+        data[index] = value;
+        int tmpIndex = index;
+
+        for (int i = 0; i < tmpArray.length; i++) {
+            data[++tmpIndex] = tmpArray[i];
+        }
+
+        size++;
     }
 
     private void ensureCapacity(int futureCapacity) {
         if (data.length <= futureCapacity) {
-            Object[] tmpData = new Object[data.length * 2];
+            Object[] tmpData = new Object[size * 2];
 
             for (int i = 0; i < data.length; i++) {
                 tmpData[i] = data[i];
@@ -35,6 +70,130 @@ public class CustomArrayList<T> {
     }
 
     public int size() {
-        return index;
+        return size;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean contains(Object value) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public T get(int index) {
+        if ((index < 0) && (index >= size)) {
+            throw new IllegalArgumentException("Cannot construct CustomArrayList with negative size!");
+        }
+
+        return (T) data[index];
+    }
+
+    public T remove(int index) {
+        if (index < 0 && index >= size) {
+            throw new IllegalArgumentException("Cannot construct CustomArrayList with negative size!");
+        }
+
+        Object tmp = data[index];
+
+        for (int i = index; i < size; i++) {
+            data[i] = data[i + 1];
+        }
+
+        data[size - 1] = null;
+        size--;
+        return (T) tmp;
+    }
+
+    public void clear() {
+        for (int i = size; i > 0; i--) {
+            data[i] = null;
+            size--;
+        }
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
     }
 }
--- src/homeworks/hw3/Main.java
+++ src/homeworks/hw3/Main.java
@@ -1,22 +1,58 @@
 package homeworks.hw3;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        CustomArrayList customArrayList = new CustomArrayList(4);
-        customArrayList.add(8);
-        customArrayList.add(87);
-        customArrayList.add(8);
-        customArrayList.add(8);
-        customArrayList.add(87);
-        customArrayList.add(8);
-        customArrayList.add(8);
-        customArrayList.add(87);
+        List<Integer> customArrayList = new CustomArrayList(4);
+        System.out.println("size " + customArrayList.size());
+
+        customArrayList.add(0);
+        customArrayList.add(1);
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        customArrayList.add(47);
+        customArrayList.add(5);
+        customArrayList.add(6);
+        customArrayList.add(7);
         customArrayList.add(8);
 
-
+        System.out.println("size " + customArrayList.size());
         System.out.println(customArrayList.size());
-        System.out.println(customArrayList.capacity);
+        System.out.println("size " + customArrayList.size());
+        System.out.println(customArrayList.get(2));
+        System.out.println("size " + customArrayList.size());
+        System.out.println(customArrayList.get(9));
+        System.out.println("size " + customArrayList.size());
 
+        customArrayList.add(9, 9);
 
+        System.out.println("size " + customArrayList.size());
+        System.out.println(customArrayList.get(9));
+        System.out.println("size " + customArrayList.size());
+        System.out.println(customArrayList.get(10));
+        System.out.println("size " + customArrayList.size());
+        System.out.println(customArrayList.get(11));
+        System.out.println("size " + customArrayList.size());
+        System.out.println(customArrayList.isEmpty());
+        System.out.println("size " + customArrayList.size());
+        System.out.println("CONTAINS  " + customArrayList.contains(47));
+        System.out.println("size " + customArrayList.size());
+        System.out.println(customArrayList.contains(48));
+        System.out.println("size " + customArrayList.size());
+
+        customArrayList.remove(5);
+
+        System.out.println("size " + customArrayList.size());
+        System.out.println(customArrayList.get(5));
+        System.out.println("size " + customArrayList.size());
+
+        customArrayList.clear();
+
+        System.out.println("size " + customArrayList.size());
+        System.out.println(customArrayList.isEmpty());
+        System.out.println("size " + customArrayList.size());
+        System.out.println(customArrayList.get(2));
     }
 }
